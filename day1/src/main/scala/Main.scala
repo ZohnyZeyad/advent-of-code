773f8f1fe3b8331ee13ca7f00cd550ab6e8494e6
stalky <zeyadalaaeldin20@gmail.com>
2024-12-01 19:26:47 +0100

    Solved Day 1 Part 1.

---
+++ day1/src/main/scala/Main.scala
@@ -0,0 +1,23 @@
+import scala.io.Source
+
+object Main extends App {
+
+  private val input = Source.fromFile("F:/PROGRAMMING/SCALA/advent-of-code/day1/data/input.txt")
+
+  private val lines = input.getLines()
+
+  private val (ids1, ids2) = lines
+    .map(line => line.split(" {3}").map(_.toInt))
+    .foldLeft((List[Int](), List[Int]())) {
+      case ((acc1, acc2), ids) => (acc1 :+ ids(0), acc2 :+ ids(1))
+    }
+
+  private val sum: Int =
+    ids1.sorted.zip(ids2.sorted)
+      .map { case (id1, id2) => Math.abs(id1 - id2) }
+      .sum
+
+  println(sum)
+
+  input.close()
+}
